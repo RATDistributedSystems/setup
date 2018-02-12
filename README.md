3e5f653ddd0a7a8c69818923b781e96ea79dedd0
asinha94 <asinha94@gmail.com>
2018-02-11 17:31:21 -0800

    added install script and readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# setup
+# Setup
 Combination of Scripts which gets everything running. Can be used for testing and on the lab machines
+
+## Run all Systems and Workload File
+
+All you have to do is run the file specific to your terminal/shell. This will instantiate the database in docker and everything else locally
+
+## Mac Terminal App
+
+```
+./mac-test-full-system.sh
+```
+
+## Tmux 
+
+```
+./tmux-test-full-system.sh
+```
+
+
+
---
+++ tmux-test-full-system.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Everything is simpler if were in the root directory
+cd ..
+
+# Start all the services in the prescribed order (Cassandra first)
+#tmux new-window -c "$PWD/../transactionServer/setup_cassandra_image" ./run.sh
+
+# Build all the servers
+DIRS="audit quoteserver transactionServer webserver workload-generator"
+for D in $DIRS; do
+    echo "Compiling $D"
+    cd $D && go build
+    cd ..
+done 
+
+# The DB should be started after 50 seconds, tables might not be initialised yet
+#sleep 50 
+
+tmux new-window -c "$PWD/audit" ./audit
+tmux new-window -c "$PWD/quoteserver" ./quoteserver
+tmux new-window -c "$PWD/webserver" ./webserver
+
+# Sleep a bit to wait for the servers to settle
+sleep 5
+tmux new-window -c "$PWD/transactionServer" ./transactionServer
+tmux new-window -c "$PWD/workload-generator" ./run.sh
